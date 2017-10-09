7aedb8c9b67082325b451777824093795b42ec2d
Ildar Khatipov <ildar.k@hotmail.co.uk>
2017-10-09 21:44:47 +0100

    added tests for the store_controller

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,12 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+	assert_select '#columns #side a', minimum: 4
+	assert_select '#main .entry', 3
+	assert_select 'h3', 'Book1'
+	assert_select '.price', /\€[,\d]+\.\d\d/
+	assert_select '#columns #side time', minimum: 1
+	
   end
 
 end
