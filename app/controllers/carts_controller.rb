2146ec9b3829a58185d54840912076f97392ef01
unknown <ildar.k@hotmail.co.uk>
2017-10-18 14:02:45 +0100

    added some error handling

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,5 @@
 class CartsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart  
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
 
   # GET /carts
@@ -70,5 +71,11 @@ class CartsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def cart_params
       params.fetch(:cart, {})
+    end
+    
+    # redirect to store incase a invalid cart url is entered
+    def invalid_cart
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, notice: 'Invalid cart'
     end
 end
