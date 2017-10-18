5c9b31b2e93cf675f37b7b441cd1a695afef3097
unknown <ildar.k@hotmail.co.uk>
2017-10-18 13:46:34 +0100

    added migration to combine items in cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018100242) do
+ActiveRecord::Schema.define(version: 20171018103909) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
